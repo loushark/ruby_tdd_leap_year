494044a8c4dfa5ee81ef74499a4d26e0a52ca360
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-27 14:57:32 +0100

    logic for divisible or not divisble by 400, 4 and 100

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,10 +1,14 @@
 class LeapYear
 
   def is_a_leap_year?(year)
-    if year % 4 != 0
-      false
-    elsif year % 4 == 0
+    if year % 4 == 0 && year % 100 != 0
       true
+    elsif year % 400 != 0
+      false
+    elsif year % 400 == 0
+      true
+    elsif year % 100 == 0
+      false
     end
   end
 end
