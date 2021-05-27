1acf47eae896e480b2eeeaa90dd6e59058d7ead0
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-27 14:41:52 +0100

    if statement for tested years 2000 and 1700 returning true or false respectively

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,6 +1,10 @@
 class LeapYear
 
   def is_a_leap_year?(year)
-    true
+    if year == 2000
+      true
+    elsif year == 1700
+      false
+    end
   end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -10,4 +10,12 @@ describe LeapYear do
       expect(test_leapYear.is_a_leap_year?(input)).to eq output
     end
   end
+
+  context 'is not a leap Year' do
+    it 'returns false when a non leap year is added' do
+      input = 1700
+      output = false
+      expect(test_leapYear.is_a_leap_year?(input)).to eq output
+    end
+  end
 end
