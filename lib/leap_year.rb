609e76270f2f001bf26c55c265bdc756f9f43fe2
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-27 14:59:26 +0100

    refactored logic

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,13 +1,13 @@
 class LeapYear
 
   def is_a_leap_year?(year)
-    if year % 4 == 0 && year % 100 != 0
+    if year % 400 == 0
       true
-    elsif year % 400 != 0
+    elsif year % 100 == 0 && year % 400 != 0
       false
-    elsif year % 400 == 0
+    elsif year % 4 == 0 && year % 100 != 0
       true
-    elsif year % 100 == 0
+    elsif year % 4 != 0
       false
     end
   end
