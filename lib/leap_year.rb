279b0a148daa621da6774c15f0fc57d852873903
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-27 15:21:08 +0100

    list_leap_years method returning array of leap years between given start and end years

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -11,4 +11,10 @@ class LeapYear
       false
     end
   end
+
+  def list_leap_years(start_year, end_year)
+    (start_year..end_year).filter_map do |year|
+      year if is_a_leap_year?(year)
+    end
+  end
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -3,31 +3,42 @@ require 'leap_year'
 describe LeapYear do
   let(:test_leapYear) { LeapYear.new }
 
-  context 'is a leap Year' do
-    it 'returns true when a leap year is added' do
-      input = 2000
-      output = true
-      expect(test_leapYear.is_a_leap_year?(input)).to eq output
+  describe '#is_a_leap_year?' do
+    context 'is a leap Year' do
+      it 'returns true when a leap year is added' do
+        input = 2000
+        output = true
+        expect(test_leapYear.is_a_leap_year?(input)).to eq output
+      end
+
+      it 'returns true when a leap year is added' do
+        input = 2008
+        output = true
+        expect(test_leapYear.is_a_leap_year?(input)).to eq output
+      end
     end
 
-    it 'returns true when a leap year is added' do
-      input = 2008
-      output = true
-      expect(test_leapYear.is_a_leap_year?(input)).to eq output
+    context 'is not a leap Year' do
+      it 'returns false when a non leap year is added' do
+        input = 1700
+        output = false
+        expect(test_leapYear.is_a_leap_year?(input)).to eq output
+      end
+
+      it 'returns false when a non leap year is added' do
+        input = 2009
+        output = false
+        expect(test_leapYear.is_a_leap_year?(input)).to eq output
+      end
     end
   end
 
-  context 'is not a leap Year' do
-    it 'returns false when a non leap year is added' do
-      input = 1700
-      output = false
-      expect(test_leapYear.is_a_leap_year?(input)).to eq output
-    end
-
-    it 'returns false when a non leap year is added' do
-      input = 2009
-      output = false
-      expect(test_leapYear.is_a_leap_year?(input)).to eq output
+  describe '#list_leap_years' do
+    it 'returns an array of three leap years between the years 2000 and 2010' do
+      input1 = 2000
+      input2 = 2010
+      output = [2000, 2004, 2008]
+      expect(test_leapYear.list_leap_years(input1, input2)).to eq output
     end
   end
 end
